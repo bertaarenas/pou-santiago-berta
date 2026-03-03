254609557d0f0639b37b3a0deec136886e1a1435
bertaarenas <berta.arenas2007@gmail.com>
2026-03-03 13:53:35 +0100

    hola

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
-
     public static void main (String[] args){
 
         Pou pou1 = new Pou("pepe", 100, 100, 100, 100, 100);
 
         System.out.println(pou1.toString());
+
+        System.out.println("hola");
 
     }
 }
