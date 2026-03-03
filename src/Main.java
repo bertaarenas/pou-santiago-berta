64f6e736bf18872cabdcfe00f145190d73300426
bertaarenas <berta.arenas2007@gmail.com>
2026-03-03 14:35:19 +0100

    .

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
 public class Main {
     public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
 
         Pou pou1 = new Pou("pepe", 100, 100, 100, 100, 100);
 
         System.out.println(pou1.toString());
 
-        System.out.println("hola");
+        System.out.println("-- Bienvenido al Pou --");
+    }
+    public static int menu(Scanner sc, int opcion) {
+        System.out.println("1. Dar de comer");
+        System.out.println("2. Dormir");
+        System.out.println("3. Jugar");
+        System.out.println("4. Tienda");
+        System.out.println("5. Estado");
+        System.out.println("6. Salir");
+        System.out.print("Elige una opción: ");
 
+        int opcion = sc.nextInt();
     }
 }
